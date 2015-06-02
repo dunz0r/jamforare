d074255ccee1cf24527865620a5ca584f65d62bb
Gabriel Fornaeus <gf@hax0r.se>
2015-06-02 10:38:41 +0200

    First code snippet, procfile

---
+++ application.py
@@ -0,0 +1,20 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8
+#
+# Copyright © 2015 Gabriel Fornaeus <gf@hax0r.se>
+#
+# Distributed under terms of the GPLv3 license.
+
+"""
+This is an application which compares compare-sites
+"""
+
+from flask import Flask
+import os
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return 'Hello from flask and stuff'
